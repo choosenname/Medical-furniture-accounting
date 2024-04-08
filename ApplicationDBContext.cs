80d1b93860144b7cb0e30a4135500a6197e56a44
Vlad <vlad@luckashonok.ru>
2024-04-09 01:21:56 +0300

    feat: add SQLite

---
+++ ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using MedicalFurnitureAccounting.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalFurnitureAccounting;
+
+public class ApplicationDBContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Supplier> Suppliers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Material> Materials { get; set; }
+    public DbSet<Supply> Supplies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=identifier.sqlite");
+    }
+}
