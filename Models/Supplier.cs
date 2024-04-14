205ad8286de4a32dd7407cba849d936ef4f42afd
Vlad <vlad@luckashonok.ru>
2024-04-14 18:30:02 +0300

    feat: update db

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -4,6 +4,7 @@ public class Supplier
 {
     public int SupplierId { get; set; }
     public string Name { get; set; }
+    public string Password { get; set; }
     // Другие свойства поставщика
 
     public ICollection<Product> Products { get; set; }
