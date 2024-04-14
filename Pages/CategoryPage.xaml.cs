028c69426c5410c508fea9c4de2f23c55e2310a4
semenyk roman <xxsamgelik@mail.ru>
2024-04-15 01:01:03 +0300

    сортировка, фильтрация, поиск

--- Pages/CategoryPage.xaml.cs
+++ Pages/CategoryPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using MedicalFurnitureAccounting.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace MedicalFurnitureAccounting.Pages;
 
@@ -21,6 +24,7 @@ public partial class CategoryPage : Page
     private void LoadCategories()
     {
         Categories = new ObservableCollection<Category>(_context.Categories.ToList());
+        FillCategoryFilterComboBox();
         DataContext = this;
     }
 
@@ -36,5 +40,102 @@ public partial class CategoryPage : Page
         DataContext = null;
         DataContext = this;
     }
+    private void SearchButton_Click(object sender, RoutedEventArgs e)
+    {
+        string searchText = searchBox.Text;
+        ICollectionView view = CollectionViewSource.GetDefaultView(categoryListView.ItemsSource);
+
+        if (!string.IsNullOrEmpty(searchText))
+        {
+            view.Filter = item =>
+            {
+                // Здесь вы можете настроить поиск по нужным полям категории
+                // Например, если хотите искать по имени, замените "CategoryId" на "Name"
+                return ((Category)item).Name.ToString().Contains(searchText);
+            };
+        }
+        else
+        {
+            view.Filter = null;
+        }
+    }
+    private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+        if (searchBox.Text == "Search")
+        {
+            searchBox.Text = "";
+        }
+    }
+
+    private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(searchBox.Text))
+        {
+            searchBox.Text = "Search";
+        }
+    }
+    private void ShowAllButton_Click(object sender, RoutedEventArgs e)
+    {
+        ICollectionView view = CollectionViewSource.GetDefaultView(categoryListView.ItemsSource);
+        view.Filter = null; // Установите фильтр на null, чтобы отобразить все элементы
+    }
+
+    private void SortByNameButton_Click(object sender, RoutedEventArgs e)
+    {
+        ICollectionView view = CollectionViewSource.GetDefaultView(categoryListView.ItemsSource);
+        if (view != null)
+        {
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+        }
+    }
+
+    private void SortByIDButton_Click(object sender, RoutedEventArgs e)
+    {
+        ICollectionView view = CollectionViewSource.GetDefaultView(categoryListView.ItemsSource);
+        if (view != null)
+        {
+            view.SortDescriptions.Clear();
+            view.SortDescriptions.Add(new SortDescription("CategoryId", ListSortDirection.Ascending));
+        }
+    }
+
+    private void FillCategoryFilterComboBox()
+    {
+        using (var context = new ApplicationDBContext()) // Замените YourDbContext на ваш контекст базы данных
+        {
+            var categoryNames = context.Categories.Select(category => category.Name).ToList();
+            categoryFilterComboBox.ItemsSource = categoryNames;
+        }
+    }
+
+
+    private void CategoryFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        string selectedCategory = categoryFilterComboBox.SelectedItem as string;
+
+        ICollectionView view = CollectionViewSource.GetDefaultView(categoryListView.ItemsSource);
+        if (view != null)
+        {
+            if (!string.IsNullOrEmpty(selectedCategory))
+            {
+                view.Filter = item =>
+                {
+                    if (item is Category category) // Предполагается, что тип элементов вашего списка - это Category
+                    {
+                        return category.Name.Equals(selectedCategory);
+                    }
+                    return false;
+                };
+            }
+            else
+            {
+                view.Filter = null; // Если категория не выбрана, отключаем фильтрацию
+            }
+        }
+    }
+
+
+
 
 }
--- Pages/ProductsPage.xaml.cs
+++ Pages/ProductsPage.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,7 +37,101 @@ namespace MedicalFurnitureAccounting.Pages
         private void LoadCategories()
         {
             Products = new ObservableCollection<Product>(_context.Products.ToList());
+            FillProductFilterComboBox();
             DataContext = this;
+        }
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchText = searchBox.Text;
+            ICollectionView view = CollectionViewSource.GetDefaultView(productsListView.ItemsSource);
+
+            if (!string.IsNullOrEmpty(searchText))
+            {
+                view.Filter = item =>
+                {
+                    return ((Product)item).Name.ToString().Contains(searchText);
+                };
+            }
+            else
+            {
+                view.Filter = null;
+            }
+        }
+
+        private void SearchBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (searchBox.Text == "Search")
+            {
+                searchBox.Text = "";
+            }
+        }
+
+        private void SearchBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(searchBox.Text))
+            {
+                searchBox.Text = "Search";
+            }
+        }
+
+        private void ShowAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            ICollectionView view = CollectionViewSource.GetDefaultView(productsListView.ItemsSource);
+            view.Filter = null; // Установите фильтр на null, чтобы отобразить все элементы
+        }
+
+        private void SortByNameButton_Click(object sender, RoutedEventArgs e)
+        {
+            ICollectionView view = CollectionViewSource.GetDefaultView(productsListView.ItemsSource);
+            if (view != null)
+            {
+                view.SortDescriptions.Clear();
+                view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+            }
+        }
+
+        private void SortByIDButton_Click(object sender, RoutedEventArgs e)
+        {
+            ICollectionView view = CollectionViewSource.GetDefaultView(productsListView.ItemsSource);
+            if (view != null)
+            {
+                view.SortDescriptions.Clear();
+                view.SortDescriptions.Add(new SortDescription("ProductId", ListSortDirection.Ascending));
+            }
+        }
+
+        private void FillProductFilterComboBox()
+        {
+            using (var context = new ApplicationDBContext()) // Поменяйте ApplicationDBContext на ваш контекст базы данных
+            {
+                var productNames = context.Products.Select(product => product.Name).ToList();
+                productsFilterComboBox.ItemsSource = productNames;
+            }
+        }
+
+        private void ProductsFilterComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedMaterial = productsFilterComboBox.SelectedItem as string;
+
+            ICollectionView view = CollectionViewSource.GetDefaultView(productsListView.ItemsSource);
+            if (view != null)
+            {
+                if (!string.IsNullOrEmpty(selectedMaterial))
+                {
+                    view.Filter = item =>
+                    {
+                        if (item is Product itemType) // Замените YourItemType на ваш тип данных для элементов списка
+                        {
+                            return itemType.Name.Equals(selectedMaterial); // Здесь нужно заменить на свойство, по которому вы хотите фильтровать
+                        }
+                        return false;
+                    };
+                }
+                else
+                {
+                    view.Filter = null; // Если материал не выбран, отключаем фильтрацию
+                }
+            }
         }
     }
 }
