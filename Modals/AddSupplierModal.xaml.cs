499f997675770c60fc1b582a18d7d6605d79469d
Vlad <vlad@luckashonok.ru>
2024-04-15 11:24:14 +0300

    feat: add AddSupplierModal

---
+++ Modals/AddSupplierModal.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using MedicalFurnitureAccounting.Models;
+
+namespace MedicalFurnitureAccounting.Modals;
+
+public partial class AddSupplierModal : Window
+{
+    public string Name { get; private set; }
+
+    public AddSupplierModal()
+    {
+        InitializeComponent();
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Получаем имя категории из текстового поля
+        Name = SupplierNameTextBox.Text;
+        // Закрываем окно
+        DialogResult = true;
+    }
+
+    private void CancelButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Закрываем окно
+        DialogResult = false;
+    }
+}
