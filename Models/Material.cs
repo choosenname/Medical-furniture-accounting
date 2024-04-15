a9480c4e9ddfc84934f258fc0d729f55aa47b6da
Vlad <vlad@luckashonok.ru>
2024-04-15 12:41:48 +0300

    feat: update models

--- Models/Material.cs
+++ Models/Material.cs
@@ -4,7 +4,6 @@ public class Material
 {
     public int MaterialId { get; set; }
     public string Name { get; set; }
-    // Другие свойства материала
 
     public ICollection<Product> Products { get; set; }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -4,10 +4,11 @@ public class Product
 {
     public int ProductId { get; set; }
     public string Name { get; set; }
-    // Другие свойства товара
 
     public int CategoryId { get; set; }
     public Category Category { get; set; }
+
+    public ICollection<Material> Materials  { get; set; }
 
     public int SupplierId { get; set; }
     public Supplier Supplier { get; set; }
