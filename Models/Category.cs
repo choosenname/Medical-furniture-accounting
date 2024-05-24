bc1d94754202df3502e55d82de933fb45a611e75
Vlad <vlad@luckashonok.ru>
2024-05-24 09:00:56 +0300

    feat: add ExtraCharge

--- Models/Category.cs
+++ Models/Category.cs
@@ -5,5 +5,7 @@ public class Category
     public int CategoryId { get; set; }
     public string Name { get; set; }
 
+    public int ExtraCharge { get; set; }
+
     public virtual ICollection<Product> Products { get; set; }
 }
