eecd3aa650c0d20a33f2939dfe394de010861ac6
Vlad <vlad@luckashonok.ru>
2024-05-24 08:44:51 +0300

    feat: add fields

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,7 +7,17 @@ public class Product
 
     public int Count { get; set; }
 
-    public string Room { get; set; }
+    public string Description { get; set; }
+
+    public int Width { get; set; }
+
+    public int Height { get; set; }
+
+    public int Length { get; set; }
+
+    public int Weight { get; set; }
+
+    public int Price { get; set; }
 
     public int CategoryId { get; set; }
     public virtual Category Category { get; set; }
--- Models/Supply.cs
+++ Models/Supply.cs
@@ -5,6 +5,8 @@ public class Supply
     public int SupplyId { get; set; }
     public DateTime Date { get; set; }
 
+    public int Count { get; set; }
+
     public int SupplierId { get; set; }
     public virtual Supplier Supplier { get; set; }
 
