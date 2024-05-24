1d25bb2e87e6f9404018b0f3326e3ca44f7a0b84
semenyk roman <xxsamgelik@mail.ru>
2024-05-24 08:35:15 +0300

    fix reg

--- ApplicationDBContext.cs
+++ ApplicationDBContext.cs
@@ -12,10 +12,20 @@ public class ApplicationDBContext : DbContext
     public DbSet<Supply> Supplies { get; set; }
     public DbSet<Storekeeper> Storekeepers { get; set; }
 
+    public ApplicationDBContext()
+    {
+        //Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=MedicalFurnitureAccounting.db");
         optionsBuilder.UseLazyLoadingProxies();
     }
-
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Storekeeper>().HasData(
+            new Storekeeper { StorekeeperId = 1, Name = "Admin",Password= "Admin" });
+    }
 }
