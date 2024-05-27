224fbb497932279d4be34274ecd07e6140b9380c
Vlad <vlad@luckashonok.ru>
2024-05-27 19:24:39 +0300

    feat: количество, в продукте должно изменяться количество

--- Models/Supply.cs
+++ Models/Supply.cs
@@ -11,4 +11,12 @@ public class Supply
     public virtual Supplier Supplier { get; set; }
 
     public virtual ICollection<Product> Products { get; set; }
+
+    public int CountTotalProductCount
+    {
+        get
+        {
+            return Products.Sum(product => product.Count);
+        }
+    }
 }
